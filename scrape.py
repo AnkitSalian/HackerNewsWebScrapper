97a1f3d5478a30870afc714d95614aa98f6a304a
Ankit Salian <ankitsalian9@gmail.com>
2020-07-18 21:18:15 +0530

    Bug fix when there is no votes mentioned

--- scrape.py
+++ scrape.py
@@ -1,21 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
 # print(soup.find_all('a'))
 links = soup.select('.storylink')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
-def custom_hacker_news(links, votes):
+def custom_hacker_news(links, subtext):
     hm = []
     for id, item in enumerate(links):
-        title = links[id].getText()
-        href = links[id].get('href', None) # If link is broken replace it with None
-        points = int(votes[id].getText().split(' ')[0])
-        if points >= 100:
-            hm.append({'title': title,'link': href, 'point': points})
+        title = item.getText()
+        href = item.get('href', None) # If link is broken replace it with None
+        vote = subtext[id].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().split(' ')[0])
+            if points >= 100:
+                hm.append({'title': title,'link': href, 'point': points})
     return hm
 
-print(custom_hacker_news(links, votes))
+pprint.pprint(custom_hacker_news(links, subtext))

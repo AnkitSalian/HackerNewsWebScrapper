19140fea63911b2b5664b101324803ca16046bbc
Ankit Salian <ankitsalian9@gmail.com>
2020-07-18 20:36:56 +0530

    Storing the data of custom hacker news in which votes are above or equal to 100

--- scrape.py
+++ scrape.py
@@ -7,3 +7,15 @@ soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup.find_all('a'))
 links = soup.select('.storylink')
 votes = soup.select('.score')
+
+def custom_hacker_news(links, votes):
+    hm = []
+    for id, item in enumerate(links):
+        title = links[id].getText()
+        href = links[id].get('href', None) # If link is broken replace it with None
+        points = int(votes[id].getText().split(' ')[0])
+        if points >= 100:
+            hm.append({'title': title,'link': href, 'point': points})
+    return hm
+
+print(custom_hacker_news(links, votes))

873d854c80fae527e1accd881d92384b56199efe
Ankit Salian <ankitsalian9@gmail.com>
2020-07-18 21:38:33 +0530

    Sort the news according to votes

--- scrape.py
+++ scrape.py
@@ -9,6 +9,9 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
 
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['point'], reverse=True)
+
 def custom_hacker_news(links, subtext):
     hm = []
     for id, item in enumerate(links):
@@ -19,6 +22,6 @@ def custom_hacker_news(links, subtext):
             points = int(vote[0].getText().split(' ')[0])
             if points >= 100:
                 hm.append({'title': title,'link': href, 'point': points})
-    return hm
+    return sort_stories_by_votes(hm)
 
 pprint.pprint(custom_hacker_news(links, subtext))

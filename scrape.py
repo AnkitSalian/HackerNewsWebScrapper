b68e56005dca65c5cda1ebad13b49b06b9fe743f
Ankit Salian <ankitsalian9@gmail.com>
2020-07-18 20:07:59 +0530

    Parsing news headline and scores list

--- scrape.py
+++ scrape.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
-print(soup.find_all('a'))
+# print(soup.find_all('a'))
+links = soup.select('.storylink')
+votes = soup.select('.score')

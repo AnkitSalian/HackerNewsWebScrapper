93c8b83e7b8d4dce17b4ebe113423de835fcec93
Ankit Salian <ankitsalian9@gmail.com>
2020-07-18 22:03:53 +0530

    Modified scrapping to accept number of pages to be scraped

--- scrape.py
+++ scrape.py
@@ -1,13 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint
+import sys
 
-res = requests.get('https://news.ycombinator.com/news')
-soup = BeautifulSoup(res.text, 'html.parser')
+def get_hacker_news(page_no):
+    res = requests.get('https://news.ycombinator.com/news?p=1')
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-# print(soup.find_all('a'))
-links = soup.select('.storylink')
-subtext = soup.select('.subtext')
+    # print(soup.find_all('a'))
+    links = soup.select('.storylink')
+    subtext = soup.select('.subtext')
+    return custom_hacker_news(links, subtext)
 
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key= lambda k:k['point'], reverse=True)
@@ -22,6 +25,17 @@ def custom_hacker_news(links, subtext):
             points = int(vote[0].getText().split(' ')[0])
             if points >= 100:
                 hm.append({'title': title,'link': href, 'point': points})
-    return sort_stories_by_votes(hm)
+    return hm
 
-pprint.pprint(custom_hacker_news(links, subtext))
+
+try:
+    # No of pages scrapping to be done
+    total_pages = int(sys.argv[1])
+    mega_news = []
+    for i in range(total_pages):
+        mega_news += get_hacker_news(i+1)
+    sorted_mega_news = sort_stories_by_votes(mega_news)
+    pprint.pprint(sorted_mega_news)
+    print(len(sorted_mega_news))
+except ValueError:
+    print('Pass a valid number greater than zero')

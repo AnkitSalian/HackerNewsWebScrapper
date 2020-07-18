cc331dc357e93633bb758c221c38b65e7e32dae1
Ankit Salian <ankitsalian9@gmail.com>
2020-07-18 19:51:14 +0530

    Convert raw text to parsable HTML using BeautifulSoup

--- scrape.py
+++ scrape.py
@@ -2,5 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(res.text, 'html.parser')
 
-print(res.text)
+print(soup.find_all('a'))
